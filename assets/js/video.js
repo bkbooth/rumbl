272c63c3beb58420786240f189130a5b5f91e25c
Ben Booth <ben.booth@gbst.com>
2018-08-22 14:27:26 +1000

    Send and handle an interval ping message on the channel

--- assets/js/video.js
+++ assets/js/video.js
@@ -21,6 +21,8 @@ let Video = {
     vidChannel.join()
       .receive("ok", resp => console.log("joined the video channel", resp))
       .receive("error", reason => console.log("join failed", reason))
+
+    vidChannel.on("ping", (data) => console.log("PING", data.count))
   },
 }
 
